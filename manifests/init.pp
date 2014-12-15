7c20c86f9fd10a5afef6c84a3f9438c0a981e75a
Denis Krienbühl <denis@href.ch>
2014-12-15 10:23:07 +0100

    Use the latest ST3 beta build

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class sublime_text_3($cmdname='subl') {
 
   package { 'SublimeText3':
     provider => 'appdmg',
-    source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203061.dmg';
+    source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg'
   }
 
   file { [
